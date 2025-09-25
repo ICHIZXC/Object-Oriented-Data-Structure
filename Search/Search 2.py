57171f194cd5deb7282b7e669ed0096ee9002219
Nathtawat Norasatthrirakul <87465662+ICHIZXC@users.noreply.github.com>
2025-09-26 00:55:46 +0700

    Update Search 2.py

--- Search/Search 2.py
+++ Search/Search 2.py
@@ -9,15 +9,15 @@ for i in inp[1].split():
         cost += index + 1
         temp = shelf.pop(index)
         shelf.insert(0, temp)
-        print(f"Search {i} -> found at {index + 1} move to front -> {" ".join(shelf)}")
+        print(f"Search {i} -> found at {index + 1} move to front ->  {' '.join(shelf)}")
     elif i not in shelf and i not in cashier:
         cost += len(shelf) + 1
         cashier.append(i)
-        print(f"Search {i} -> not found -> {" ".join(shelf)}")
+        print(f"Search {i} -> not found -> {' '.join(shelf)}")
     elif i in cashier:
         shelf.insert(0, i)
         cost += 1
-        print(f"Search {i} -> add new book -> {" ".join(shelf)}")
+        print(f"Search {i} -> add new book ->  {' '.join(shelf)}")
         
-print(f"\nFinal books: {" ".join(shelf)}")
+print(f"\nFinal books: {' '.join(shelf)}")
 print(f"Total cost: {cost}")
